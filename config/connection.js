113b0012acf9e492f03464a37dfceca291296e07
Cody Michaud <cody.r.michaud@gmail.com>
2021-03-30 10:28:26 -0700

    Setup config folder

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,16 @@
-require('dotenv').config()
+require('dotenv').config();
+
+const sequelize = require('sequelize');
+
+const sequalize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+    }
+);
+
+module.exports = sequalize;
