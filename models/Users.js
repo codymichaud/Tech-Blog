71dd2c5949329555c7ae8d8d5e32d49281db3d81
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-01 11:07:40 -0700

    Added dashboard model, coded index.js model and users.js model

--- models/Users.js
+++ models/Users.js
@@ -1,1 +1,60 @@
-const { Model, DataTypes } = require
+const { Model, DataTypes } = require('sequelize');
+const bcryptjs = require('bcryptjs');
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkUserPass(loginPass) {
+        return bcryptjs.compareSync(loginPass, this.password);
+    }
+};
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [9],
+            },
+        },
+    },
+    //adding hooks
+    {
+        hooks: {
+            beforeCreate: async (newUserData) => {
+                newUserData.password = await bcryptjs.hash(newUserData.password, 10);
+                return newUserData;
+            },
+            beforeUpdate: async (updatedUserData) => {
+                updatedUserData.password = await bcryptjs.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            },
+        },
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+
+);
+
+module.exports = User;
--- models/index.js
+++ models/index.js
@@ -1,1 +1,13 @@
 const User = require('./Users');
+const Dashboard = require('./Dashboards');
+
+User.hasMany(Dashboard, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Dashboard.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+module.exports = { User, Dashboard };
