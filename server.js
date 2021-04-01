fd6a1707c1f41aeaa150e3c1bb6f63bc2c7295f5
Cody Michaud <cody.r.michaud@gmail.com>
2021-03-31 21:25:34 -0700

    Installing dependencies

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const routes = require('./routes');
+const exphbs = require('express-handlebars');
+const session = require('express-session');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
