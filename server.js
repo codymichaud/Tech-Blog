bffb006cd717da26d767ac03fd20564dcedaccf8
Cody Michaud <cody.r.michaud@gmail.com>
2021-03-31 21:56:44 -0700

    Added session and server for app

--- server.js
+++ server.js
@@ -2,16 +2,29 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const path = require('path');
-const sequelize = require('./config/connection')
+const sequelize = require('./config/connection');
+const handlebars = require('handlebars');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sess = {
+    secret: 'Super super secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({ db: sequelize })
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session(sess));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(routes);
 
-app.listen(PORT, () => {
-    console.log(`App listening on ${PORT}`);
-});
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Client now listening on PORT ${PORT}`);
+    })
+})
