8c57ffedf4b7e7d9afb44eb5daa0f31ee58d9357
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-05 10:11:08 -0700

    Debugging sequelize access denied error. .env file is correct

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ const path = require('path');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
 const sess = {
     secret: 'Super super secret',
     cookie: {},
@@ -17,14 +13,17 @@ const sess = {
     store: new SequelizeStore({ db: sequelize })
 };
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session(sess));
 app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
 
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => {
-        console.log(`Client now listening on PORT ${PORT}`);
-    })
+    app.listen(PORT, () => console.log(`Client now listening on PORT ${PORT}`));
 })
