b2594dc333eb2831c48d03ce85114a252bad0284
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-05 13:16:19 -0700

    Finished Comments model

--- models/comments.js
+++ models/comments.js
@@ -4,8 +4,37 @@ const { Model, DataTypes } = require('sequelize');
 class Comments extends Model { }
 
 Comments.init({
-    id: {},
-    comments_txt: {},
-    user_id: {},
-    posts_id: {},
-})
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    comments_txt: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: 'users',
+            key: 'id'
+        }
+    },
+    posts_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+            model: 'posts',
+            key: 'id',
+        }
+    },
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comments',
+});
+
+module.exports = Comments;
