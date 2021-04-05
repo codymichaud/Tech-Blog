d5ee6a13cee064ab1a7aeb89360dd4565c53e343
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-05 12:56:34 -0700

    Starting on Comments models

--- models/comments.js
+++ models/comments.js
@@ -1,1 +1,11 @@
 const sequelize = require('../config/connection');
+const { Model, DataTypes } = require('sequelize');
+
+class Comments extends Model { }
+
+Comments.init({
+    id: {},
+    comments_txt: {},
+    user_id: {},
+    posts_id: {},
+})
