3f1efcd1a3720de775544ed45b292471b7af3416
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-05 12:31:28 -0700

    Fininshed Posts model

--- models/Posts.js
+++ models/Posts.js
@@ -1,3 +1,36 @@
 const sequelize = require('../config/connection');
 const { Model, Datatypes } = require('sequelize');
 
+
+class Posts extends Model { }
+
+Posts.init({
+    id: {
+        type: Datatypes.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    title: {
+        type: Datatypes.STRING,
+        allowNull: false,
+    },
+    body: {
+        type: Datatypes.STRING,
+        allowNull: false,
+    },
+    user_id: {
+        type: Datatypes.INTEGER,
+        references: {
+            model: 'users',
+            key: 'id',
+        },
+    },
+}, {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'posts'
+})
+
+module.exports = Posts;
