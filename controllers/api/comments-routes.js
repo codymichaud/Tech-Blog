431f840fb0c63df0517624b6c1e9579b4355d28a
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-07 09:24:06 -0700

    Debugging database error. says access denied but says im using the correct password. Moving to routes for a little bit

---
+++ controllers/api/comments-routes.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+const { Comments, Posts, Users } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+
+})
