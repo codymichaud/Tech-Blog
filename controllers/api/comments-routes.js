9c51140683e9bbce43033f559b5b6175f3a91fbb
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-07 09:38:38 -0700

    Finished first route for comments

--- controllers/api/comments-routes.js
+++ controllers/api/comments-routes.js
@@ -3,5 +3,17 @@ const { Comments, Posts, Users } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 router.get('/', async (req, res) => {
+    try {
+        const commentsData = await Comments.findAll({
+            include: [{
+                model: Users,
+                as: 'user',
+                attributes: ['name'],
+            },
+            ],
+        });
+    } catch (error) {
+        res.status(400).json(error);
+    }
+});
 
-})
