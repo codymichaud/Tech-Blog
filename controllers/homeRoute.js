6ce01cbd8ca3cf8b5cbe63380a0f72292d9db202
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-27 09:11:54 -0700

    Deleting extra routes files. setting up routes

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,16 +1,23 @@
 const router = require('express').Router();
-const { json } = require('sequelize/types');
-const withAuth = require('../utils/auth');
+const { User, Post, Comment } = require('../models');
+const sequelize = require('../config/connection');
 
 
 router.get('/', async (req, res) => {
     try {
+        const postData = await Post.findAll({
+            include: [User],
+        });
 
+        const post = postData.map((posts) => post.get({ plain: true }));
+
+        res.render('all-posts', {
+            post
+        });
     } catch (error) {
-
+        res.status(500).json(error);
     }
 });
-
 
 router.get('/dashboard', withAuth, async (req, res) => {
     try {
@@ -30,3 +37,27 @@ router.get('/login', async (req, res) => {
     }
 
 });
+
+router.get('/post/:id', async (req, res) => {
+    try {
+        const postData = await Post.findbyPk(req.params.id, {
+            include: [
+                User, {
+                    model: Comment,
+                    inlude: [User]
+                }
+            ],
+        });
+
+        if (postData) {
+            const post = postData.get({ plain: true });
+            res.render('single-post', {
+                post
+            });
+        } else {
+            res.status(404).end();
+        }
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
