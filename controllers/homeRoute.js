e7d87333635e1b4845149c7ae1b79795cd25617b
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-29 20:23:37 -0700

    Added find all attributes to the homeRoute

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -4,5 +4,18 @@ const sequelize = require('../config/connection');
 
 
 router.get('/', (req, res) => {
-
+    Post.findAll({
+        attributes: ['id', 'title', 'body', 'user_id'],
+        include: [{
+            model: User,
+            as: 'user',
+            attributes: ['username'],
+        },
+        {
+            model: Comment,
+            as: 'comments',
+            attributes: ['id', 'comment_text', 'user_id'],
+        },
+        ]
+    })
 })
