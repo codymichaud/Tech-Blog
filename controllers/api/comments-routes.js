0f0d7259d5239af17b9cf6d6701c19092319e0a8
Cody Michaud <cody.r.michaud@gmail.com>
2021-04-29 20:10:31 -0700

    Restarting home routes

--- controllers/api/comments-routes.js
+++ controllers/api/comments-routes.js
@@ -2,22 +2,7 @@ const router = require('express').Router();
 const { Comments, Posts, Users } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.get('/', async (req, res) => {
-    try {
-        const commentsData = await Comments.findAll({
-            include: [{
-                model: Users,
-                as: 'user',
-                attributes: ['name'],
-            },
-            ],
-        });
 
-
-    } catch (error) {
-        res.status(400).json(error);
-    }
-});
 
 router.post('/', async (req, res) => {
 
