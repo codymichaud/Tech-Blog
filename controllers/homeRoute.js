6502080934353197226ef39b52cb747dcaffda19
Cody Michaud <cody.r.michaud@gmail.com>
2021-05-10 09:46:28 -0700

    Adding routes to homeRoute

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
-const { User, Post, Comment } = require('../models');
+const { Users, Posts, Comments } = require('../models');
 const sequelize = require('../config/connection');
 
 
 router.get('/', (req, res) => {
-    Post.findAll({
+    Posts.findAll({
         attributes: ['id', 'title', 'body', 'user_id'],
         include: [{
             model: User,
@@ -37,3 +37,29 @@ router.get('/', (req, res) => {
             res.status(500).json(error);
         });
 });
+
+router.get('/', (req, res) => {
+    Posts.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: ['id', 'title', 'body', 'user_id'],
+        include: [{
+            model: Users,
+            as: 'user',
+            attributes: ['username']
+        },
+        {
+            model: Comments,
+            as: 'comments',
+            attributes: ['id', 'comment_text', 'user_id'],
+            include: [{
+                model: Users,
+                as: 'users',
+                attributes: 'username',
+
+            }]
+        }
+        ]
+    })
+})
