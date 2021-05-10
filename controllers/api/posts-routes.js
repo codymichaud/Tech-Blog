7d2b7532abfd17cc2b0150eb0433a5c003fcf8f5
Cody Michaud <cody.r.michaud@gmail.com>
2021-05-10 15:07:02 -0700

    Finished posts-routes.js. Moving to comments-routes.js

--- controllers/api/posts-routes.js
+++ controllers/api/posts-routes.js
@@ -18,3 +18,68 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/:id', (req, res) => {
+    Posts.findOne({
+        where: {
+            id: req.params.id,
+        },
+        attributes: ['id', 'title', 'body', 'user_id'],
+        include: [{
+            model: Comments,
+            as: 'comments',
+            attributes: ['id', 'comment_body', 'user_id'],
+        }]
+    })
+        .then((dbPostData) => {
+            if (!dbPostData) {
+                res.status(404).json({
+                    message: 'Sorry there are no posts found with this id'
+                });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch((error) => {
+            res.status(500).json(error);
+        });
+});
+
+router.post('/', (req, res) => {
+    Posts.create({
+        title: req.body.title,
+        body: req.body.body,
+        user_id: req.session.user_id,
+    })
+        .then((dbPostData) => {
+            res.json(dbPostData);
+        })
+        .catch((error) => {
+            res.status(500).json(error);
+        });
+});
+
+router.put(':id', (req, res) => {
+    Posts.update({
+        title: req.body.title,
+        body: req.body.body,
+    },
+        {
+            where: {
+                id: req.params.id,
+            },
+        })
+        .then((dbPostData) => {
+            if (dbPostData) {
+                res.status(404).json({
+                    message: 'Sorry there are no posts found with this id'
+                });
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch((error) => {
+            res.json(error);
+        });
+});
+
+module.exports = router;
