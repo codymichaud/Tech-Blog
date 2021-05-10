b1ae5fcd056a0e41ad4c1bace154cb39eefd8751
Cody Michaud <cody.r.michaud@gmail.com>
2021-05-10 08:53:58 -0700

    Editing homeRoutes

--- controllers/homeRoute.js
+++ controllers/homeRoute.js
@@ -16,6 +16,24 @@ router.get('/', (req, res) => {
             as: 'comments',
             attributes: ['id', 'comment_text', 'user_id'],
         },
-        ]
+        ],
     })
-})
+        .then((dbPostData) => {
+            if (!dbPostData) {
+                res.status(404).json({
+                    message: 'There are no posts available to view'
+                });
+                return;
+            }
+            const post = dbPostData.map((posts) => posts.get({
+                plain: true
+            }));
+            res.render('home', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch((error) => {
+            res.status(500).json(error);
+        });
+});
