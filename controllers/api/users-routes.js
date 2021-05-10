735365bb502642ddedc4c8fb45565297c948d954
Cody Michaud <cody.r.michaud@gmail.com>
2021-05-10 11:38:18 -0700

    Added first user get route. Starting get post by id route

--- controllers/api/users-routes.js
+++ controllers/api/users-routes.js
@@ -1,3 +1,26 @@
 const router = require('express').Router();
 const { Users, Posts, Comments } = require('../../models');
 
+router.get('/', (req, res) => {
+    Users.findAll({
+        attributes: ['id', 'username', 'email', 'password'],
+        include: [{
+            model: Posts,
+            as: 'posts',
+            attributes: ['id', 'title', 'body'],
+        },
+        {
+            model: Comments,
+            as: 'comments',
+            attributes: ['id', 'comment_text', 'post_id'],
+        },
+        ],
+    })
+        .then((dbUserData) => {
+            res.json(dbUserData);
+        })
+        .catch((error) => {
+            res.status(500).json(error)
+        });
+});
+
