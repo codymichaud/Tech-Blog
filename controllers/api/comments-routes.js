a2e508404944380c4eb786f5574e343022c094ee
Cody Michaud <cody.r.michaud@gmail.com>
2021-05-10 13:54:41 -0700

    Starting on posts-routes.js

--- controllers/api/comments-routes.js
+++ controllers/api/comments-routes.js
@@ -1,9 +1,9 @@
-const router = require('express').Router();
-const { Comments, Posts, Users } = require('../../models');
-const withAuth = require('../../utils/auth');
+// const router = require('express').Router();
+// const { Comments, Posts, Users } = require('../../models');
+// const withAuth = require('../../utils/auth');
 
 
 
-router.post('/', async (req, res) => {
+// router.post('/', async (req, res) => {
 
-});
+// });
---
+++ controllers/api/posts-routes.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+const { Users, Posts, Comments } = require('../../models');
+
+router.get('')
